249a3077f24612bbfc4333442bcd51c893f11d92
Laily Azizah Islami <gimamimow@gmail.com>
2026-04-22 20:25:38 +0700

    queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -2,46 +2,51 @@
 #include <stdexcept>
 
 void init(Queue* q) {
-    q->front = nullptr;
-    q->rear = nullptr;
+    q->front = -1;
+    q->rear = -1;
 }
 
 bool isEmpty(const Queue* q) {
-    return q->front == nullptr;
+    return q->front == -1;
 }
 
 bool isFull(const Queue* q) {
-    if (isEmpty(q)) return false;
-    return q->rear == &(q->data[MAX - 1]);
+    return q->rear == MAX - 1;
 }
 
 void enqueue(Queue* q, int value) {
-    if (isFull(q)) throw std::out_of_range("Queue Overflow");
-    if (isEmpty(q)) {
-        q->front = &(q->data[0]);
-        q->rear = &(q->data[0]);
-    } else {
-        q->rear++;
+    if (isFull(q)) {
+        throw std::overflow_error("Queue Overflow: Kapasitas penuh");
     }
-    *(q->rear) = value;
+    if (isEmpty(q)) {
+        q->front = 0;
+    }
+    q->rear++;
+    q->data[q->rear] = value;
 }
 
 void dequeue(Queue* q) {
-    if (isEmpty(q)) throw std::underflow_error("Queue Underflow");
-    if (q->front == q->rear) {
-        q->front = nullptr;
-        q->rear = nullptr;
+    if (isEmpty(q)) {
+        throw std::underflow_error("Queue Underflow: Antrean kosong");
+    }
+    if (q->front >= q->rear) {
+        q->front = -1;
+        q->rear = -1;
     } else {
         q->front++;
     }
 }
 
 int front(const Queue* q) {
-    if (isEmpty(q)) throw std::underflow_error("Queue Empty");
-    return *(q->front);
+    if (isEmpty(q)) {
+        throw std::underflow_error("Antrean kosong");
+    }
+    return q->data[q->front];
 }
 
 int back(const Queue* q) {
-    if (isEmpty(q)) throw std::underflow_error("Queue Empty");
-    return *(q->rear);
+    if (isEmpty(q)) {
+        throw std::underflow_error("Antrean kosong");
+    }
+    return q->data[q->rear];
 }
